49e6348bed64dca8520d981015a5061f16664429
SDen4 <asw325@yandex.ru>
2020-09-07 22:20:09 +0500

    initial commit

---
+++ src/components/Main.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="form">
+        <div class="form__wrapper">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: "message!!!"
+            }
+        }
+    }
+</script>
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles/sass/main.sass";
+
+window.Vue = require("vue");
+
+Vue.component("main-component", require("./components/Main.vue").default)
+
+
+const app = new Vue({
+    el: "#app"
+});
